2d4a7ec1d150a5b11b73907291efff4ba811702f
adson62 <adson.matheus62@gmail.com>
2021-01-21 16:01:56 -0300

    Added main.c

--- main.c
+++ main.c
@@ -15,12 +15,12 @@ void menuSobre(void) {
   printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
   printf("///               Departamento de Computação e Tecnologia                   ///\n");
   printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
-  printf("///                  Projeto Sistema de Gestão Escolar                      ///\n");
+  printf("///               Projeto Controle de Estoque de Empresas                   ///\n");
   printf("///         Developed by  @WesleyVitor and @adson62 - Jan, 2021             ///\n");
   printf("///                                                                         ///\n");
   printf("///////////////////////////////////////////////////////////////////////////////\n");
   printf("///                                                                         ///\n");
-  printf("///         = = = = = Controle de estoque de empresas = = = = =             ///\n");
+  printf("///         = = = = = Controle de Estoque de Empresas = = = = =             ///\n");
   printf("///                                                                         ///\n");
   printf("///  O controle de estoque em uma empresa é uma tarefa fundamental para o   ///\n");
   printf("///  seu funcionamento. Saber exatamente a quantidade de produtos e quais   ///\n");
