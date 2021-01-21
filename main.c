9f17dd48d5e80fb7ade40dad61c71fa2e7d478f1
adson62 <adson.matheus62@gmail.com>
2021-01-21 16:00:19 -0300

    Added main.c

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void menuSobre(void);
+
+int main(void) {
+  menuSobre();
+  return 0;
+}
+
+void menuSobre(void) {
+  printf("\n");
+  printf("///////////////////////////////////////////////////////////////////////////////\n");
+  printf("///                                                                         ///\n");
+  printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+  printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+  printf("///               Departamento de Computação e Tecnologia                   ///\n");
+  printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+  printf("///                  Projeto Sistema de Gestão Escolar                      ///\n");
+  printf("///         Developed by  @WesleyVitor and @adson62 - Jan, 2021             ///\n");
+  printf("///                                                                         ///\n");
+  printf("///////////////////////////////////////////////////////////////////////////////\n");
+  printf("///                                                                         ///\n");
+  printf("///         = = = = = Controle de estoque de empresas = = = = =             ///\n");
+  printf("///                                                                         ///\n");
+  printf("///  O controle de estoque em uma empresa é uma tarefa fundamental para o   ///\n");
+  printf("///  seu funcionamento. Saber exatamente a quantidade de produtos e quais   ///\n");
+  printf("///  deles estão disponíveis. Para isso, um software de controle de estoque ///\n");
+  printf("///  é indispensável para haja uma administração bem feita dessa área, de   ///\n");
+  printf("///  forma rápida e eficiente. Sem isso, seria praticamente impossível      ///\n");
+  printf("///  que as médias/grandes empresas obtivessem um controle dos produtos.    ///\n");
+  printf("///                                                                         ///\n");
+  printf("///////////////////////////////////////////////////////////////////////////////\n");
+  printf("\n");
+}
