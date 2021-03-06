6636fb8a883a3bbe7aa92aa277942215c7add9fd
Adson Matheus <40866921+adson62@users.noreply.github.com>
2021-03-05 22:50:46 -0300

    add: funcoes de validacao de datas

--- moduloValidacao/validacoes.h
+++ moduloValidacao/validacoes.h
@@ -9,3 +9,5 @@ int validarSegundoDigitoSubmetido(char [14]);
 int testarPrimeiroDigitoSubmetido(char[11]);
 int testarSegundoDigitoSubmetido(char[11]);
 int validarCPF(char[11]);
+int validaData(char[11]);
+int bissexto(int);
