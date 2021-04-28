3f301a960a3c0cac9cfc218879e26025b2757c74
WesleyVitor <wesleydemorais@outlook.com.br>
2021-04-28 16:38:09 -0300

    add: relatório de clientes cadastrados alfabeticamente

--- Relatorios/relatorios.c
+++ Relatorios/relatorios.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../Validacao/validacoes.h"
 #include "./relatorios.h"
+#include "../Cliente/funcoesCliente.h"
 void navegacaoMenuRelatorios(void){
   char opcao;
   do{
     opcao = menuRelatorios();
     switch(opcao){
       case '1':
-        telaRelatoriosClientes();
+        relatorioCliente();
         break;
       case '2':
         telaRelatorioProdutos();
@@ -37,7 +39,7 @@ char menuRelatorios(void){
   printf("///                                                                         ///\n");
   printf("///////////////////////////////////////////////////////////////////////////////\n");
   printf("///                                                                         ///\n");
-  printf("///               1. Relatórios de Clientes Cadastrados                     ///\n");
+  printf("///               1. Relatórios de Clientes Cadastrados Alfabeticamente     ///\n");
   printf("///               2. Relatórios de Produtos Cadastrados                     ///\n");
   printf("///               3. Relatórios de Retiradas Trimestrais                    ///\n");
   printf("///               4. Relatórios Financeiros                                 ///\n");
@@ -142,5 +144,69 @@ void telaRelatorioFinanceiro(void){
   printf("///                                                                         ///\n");
   printf("\n");
   printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+  getchar();
+}
+
+ClienteLista* gerarRelatorioCliente(){
+    FILE *fp;
+    ClienteLista *cliente;
+    ClienteLista *lista;
+    
+    lista = NULL;
+    fp = fopen("Dados/Clientes.dat","rb");
+    if (fp == NULL){
+   	 printf("Erro na abertura do arquivo... \n");
+   	 return NULL;
+    }
+    else{
+   	 cliente = (ClienteLista *) malloc(sizeof(ClienteLista));
+   	 while (fread(cliente, sizeof(Cliente), 1, fp)){
+        if ((lista == NULL) || (strcmp(cliente->nome, (lista)->nome) < 0)) {
+          cliente->prox = lista;
+          lista = cliente;
+        } else  {
+          ClienteLista* ant = lista;
+          ClienteLista* atu = (lista)->prox;
+          while ((atu != NULL) && (strcmp(atu->nome, cliente->nome) < 0)) {
+            ant = atu;
+            atu = atu->prox;
+          }
+          ant->prox = cliente;
+          cliente->prox = atu;
+        }
+        cliente = (ClienteLista *) malloc(sizeof(ClienteLista));
+   	 }
+   	 free(cliente);
+   	 fclose(fp);
+    }  
+
+    return lista;  
+}
+
+void relatorioCliente(){
+  ClienteLista* lista;
+
+  lista = gerarRelatorioCliente();
+
+  exibirLista(lista);
+  
+}
+
+void exibirLista(ClienteLista *aux){
+  printf("\n\n");
+  printf("****************************************\n");
+	printf("*** Relatório dos Clientes Cadastrados ***\n");
+  printf("****************************************\n");
+  printf("\n");
+	while (aux != NULL){
+    	printf("Nome do cliente:       %s\n", aux->nome);
+      printf("Rua:                   %s\n", aux->rua);
+      printf("Bairo:                 %s\n", aux->bairro);
+      printf("Número:                %s\n", aux->numero);
+      printf("Complemento:           %s\n", aux->complemento);
+      printf("\n\n");
+    	aux = aux->prox;
+	}
+	printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
   getchar();
 }
--- Relatorios/relatorios.h
+++ Relatorios/relatorios.h
@@ -1,6 +1,21 @@
+typedef struct clienteLista ClienteLista;
+struct clienteLista{
+  char cnpj_cpf[21];
+  char nome[51];
+  char rua[51];
+  char bairro[51];
+  char numero[7];
+  char complemento[20];
+  char status;
+  ClienteLista* prox;
+};
+
 void navegacaoMenuRelatorios(void);
 char menuRelatorios(void);
 void telaRelatoriosClientes(void);
 void telaRelatorioProdutos(void);
 void telaRelatorioRetiradasTrimestrais(void);
 void telaRelatorioFinanceiro(void);
+ClienteLista* gerarRelatorioCliente(void);
+void relatorioCliente(void);
+void exibirLista(ClienteLista*);
